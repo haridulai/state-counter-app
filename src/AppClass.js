27b8a6b49c9a82a63a4bd1dba0422f5352c32550
Hari Dulai <hdcodesdev@gmail.com>
2023-01-29 20:50:49 +0000

    creating counter app using react class component

---
+++ src/AppClass.js
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+
+export default class AppClass extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      count: 0,
+    };
+  }
+
+  increment = () => {
+    this.setState({
+      count: this.state.count + 1,
+    });
+  };
+
+  decrement = () => {
+    this.setState({
+      count: this.state.count - 1,
+    });
+  };
+
+  render() {
+    return (
+      <div className="app">
+        <h1>Wleomce to my counter app</h1>
+        <p>The count is: {this.state.count}</p>
+        <button onClick={this.decrement}>-</button>
+        <button onClick={this.increment}>+</button>
+      </div>
+    );
+  }
+}
